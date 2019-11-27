6afe6c451deb25e15779246f34ea4f4942fc4f30
j1nma <j1nma@users.noreply.github.com>
2019-11-27 02:44:42 +0100

    split and metrics

--- Exercise2.py
+++ Exercise2.py
@@ -4,6 +4,8 @@ import os
 import subprocess
 
 from sklearn import tree  # tree must have categorical_features and absolute_error implementations
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
 
 # Dataset directory
 dataset_directory = "./datasets"
@@ -46,6 +48,8 @@ np.random.seed(my_FH_id)
 criteria = ["gini", "entropy", "absolute_error"]
 
 for dictionary in dictionaries:
+    print(dictionary['dataset_name'] + "...")
+
     # Create results directory
     dirName = './results/' + dictionary['dataset_name']
     if not os.path.exists(dirName):
@@ -65,13 +69,18 @@ for dictionary in dictionaries:
     target = np.array(target.eq(dictionary['label_column_yes']).mul(1))
 
     for criterion in criteria:
+        # Split
+        X_train, X_test, y_train, y_test = train_test_split(input_samples_encoded, target, random_state=my_FH_id,
+                                                            shuffle=False)
+
         # Fit
         clf = tree.DecisionTreeClassifier(random_state=my_FH_id, criterion=criterion)
-        clf = clf.fit(input_samples_encoded, target, categorical_features=dictionary['categorical_features'])
+        clf = clf.fit(X_train, y_train, categorical_features=dictionary['categorical_features'])
 
         # Export tree
         dotPath = dirName + "/" + dictionary['dataset_name'] + '-' + criterion + '.dot'
-        tree.export_graphviz(clf, dotPath,
+        tree.export_graphviz(clf, dotPath, filled=True, rounded=True,
+                             special_characters=True,
                              feature_names=list(input_samples_encoded.columns),
                              class_names=[dictionary['label_column_no'], dictionary['label_column_yes']])
 
@@ -79,3 +88,13 @@ for dictionary in dictionaries:
         subprocess.Popen(
             ["dot", "-Tpng", dotPath, "-o", dirName + "/" + dictionary['dataset_name'] + '-' + criterion + ".png"],
             stdout=subprocess.PIPE)
+
+        # Predict
+        y_pred = clf.predict(X_test)
+
+        print('\t{} | Accuracy: {:0.2f}, Precision: {:0.2f}, Recall: {:0.2f}'.format(
+            criterion,
+            metrics.accuracy_score(y_test, y_pred),
+            metrics.precision_score(y_test, y_pred),
+            metrics.recall_score(y_test, y_pred)))
+    print('\n')
